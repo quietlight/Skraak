28df844296a723900c16b7613e1c886ddcb5139c
David Cary <getdavidcary@icloud.com>
2023-02-02 17:54:37 +1300

    new dataset function

--- src/CSVto.jl
+++ src/CSVto.jl
@@ -194,11 +194,13 @@ end
 dataset()
 
 This function takes a csv file in a folder and generates a dataset 
-with label K-Set. 
+from a vector of labels. 
+
+It is intended to be used for Male, Female, Close as it is correcting older annotations as it goes.
 
 It works on CSV's from hand labelled AvianNZ style JSON.
 
-It saves  wav files to /media/david/72CADE2ECADDEDFB/DataSet/SSSlabel/  
+It saves  wav files to /media/david/72CADE2ECADDEDFB/DataSet/$$$label/  
 and annotation files to a seperate directory as raven files. (it would be 
 better for me to just build the csv direct, soon.)
 
@@ -209,90 +211,131 @@ better for me to just build the csv direct, soon.)
     for i in x
     cd(i)
     Skraak.CSVto.dataset()
-    cd("/path/to/working/directory")
+    #cd("/path/to/working/directory")
+    cd("/media/david/Pomona-2/")
     end
 
 using Glob, CSV, DataFrames, DelimitedFiles
 """
 
-function dataset()
-    raw_data = glob("*.csv")
+function dataset(labels::Vector{String})
+    raw_data = glob("kiwi_data-*.csv")
 
-    if length(raw_data) == 1
+    if length(raw_data) != 1
+        message = pwd() * " has no kiwi_data csv present, or more than 1"
+        return message
+    
+    else
         data_frame = DataFrame(CSV.File(raw_data[1]))
-    else
-        message = pwd() * " has no csv present, or more than 1"
-        return message
-    end
 
-    if !("K-Set" in levels(data_frame.species))
-        message = pwd() * " K-Set not present"
-        return message
-    else
-        species_grouped_frames = groupby(data_frame, :species)
-        # Beware this comma!!
-        kset = species_grouped_frames[(species = :"K-Set",)]
-        path_sets = groupby(kset, :Path)
+        for row in eachrow(data_frame)
+            # correct to Male, Female, Close to match newer annotations
+            if row.species == "K-M"
+                row.species = "Male"
 
-        for file_path in eachindex(path_sets)
-            f = path_sets[file_path]
-            data = Any[]
-            headers = Any[
-                "Selection",
-                "View",
-                "Channel",
-                "start_time",
-                "end_time",
-                "low_f",
-                "high_f",
-                "Species",
-                "Notes",
-            ]
-            push!(data, headers)
-            for (index, row) in enumerate(eachrow(f))
-                push!(
-                    data,
-                    [
-                        index,
-                        "Spectrogram 1",
-                        "1",
-                        row."Start Time (relative)",
-                        row."End Time (relative)",
-                        row.min_freq,
-                        row.max_freq,
-                        row.species,
-                        "",
-                    ],
-                )
+            elseif row.species == "K-F"
+                row.species = "Female"
+
+            elseif row.species == "K-Close"
+                row.species = "Close"    
+            
+            # correct K-MF to Male plus another row Female
+            elseif row.species == "K-MF"
+                row.species = "Male"
+                new_row = row[:] # copy row             
+                new_row.species = "Female"
+                push!(data_frame, new_row)    
+
             end
-            p = split(f[1, :Path], ".")
-            if length(p) < 3
-                # Julia does not like | in file names, but all my csv files as already built with | in file path.
-                # But I need q[1], q[2] later when I save the wav anyway, so its ok
-                q = split(p[end-1], "|")
-                r = string(q[1], "_", q[2], "_", q[3])
-                output_file =
-                    "/media/david/72CADE2ECADDEDFB/DataSet/K-Set_AnnoTables/" *
-                    r *
-                    ".Table.1.selections.txt"
-            else
-                error("File names have gone to hell. One period only David")
-            end
-            open(output_file, "w") do io
-                writedlm(io, data, '\t')
-            end
-            src = chop(f[1, :File_Name], tail = 5)
-            dst =
-                "/media/david/72CADE2ECADDEDFB/DataSet/K-Set/" *
-                q[1] *
-                "_" *
-                q[2] *
-                "_" *
-                src
-            cp(src, dst, force = true)
-            print(".")
         end
+
+        println(pwd() * "\t" * levels(data_frame.species))
+
+        # Check to see if labels exist in the data frame
+        valid_labels = String[]
+
+        for label in labels
+            if !(label in levels(data_frame.species))
+                message = pwd() * """ "$label" label not present"""
+                println(message)
+            else
+                push!(valid_labels, label)
+            end
+        end
+        
+        if length(valid_labels) < 1
+            message = pwd() * " no valid labels"
+            return message
+
+        # If there are valid labels present construct the dataset
+        else
+            set = filter(:species => species -> species in valid_labels, data_frame)
+            path_sets = groupby(set, :Path)
+
+            for file_path in eachindex(path_sets)
+                f = path_sets[file_path]
+                data = Any[]
+                headers = Any[
+                    "Selection",
+                    "View",
+                    "Channel",
+                    "start_time",
+                    "end_time",
+                    "low_f",
+                    "high_f",
+                    "Species",
+                    "Notes",
+                ]
+                push!(data, headers)
+                for (index, row) in enumerate(eachrow(f))
+                    push!(
+                        data,
+                        [
+                            index,
+                            "Spectrogram 1",
+                            "1",
+                            row."Start Time (relative)",
+                            row."End Time (relative)",
+                            row.min_freq,
+                            row.max_freq,
+                            row.species,
+                            "",
+                        ],
+                    )
+                end
+                p = split(f[1, :Path], ".")
+                if length(p) < 3
+                    # Julia does not like | in file names, but all my csv files as already built with | in file path.
+                    # But I need q[1], q[2] later when I save the wav anyway, so its ok
+                    q = split(p[end-1], "|")
+                    r = string(q[1], "_", q[2], "_", q[3])
+                    output_file =
+                        "/media/david/72CADE2ECADDEDFB/DataSet/Annotations/" *
+                        r *
+                        ".Table.1.selections.txt"
+                else
+                    error("File names have gone to hell. One period only David")
+                end
+                open(output_file, "w") do io
+                    writedlm(io, data, '\t')
+                end
+                src = chop(f[1, :File_Name], tail = 5)
+                dst =
+                    "/media/david/72CADE2ECADDEDFB/DataSet/" *
+                    q[1] *
+                    "_" *
+                    q[2] *
+                    "_" *
+                    src
+                cp(src, dst, force = true)
+                print(".")
+            end
+        
+        end
+
     end
+    
+    
 end
 
 """
